0d0d1ce0019e82f62c4d0539a77851ada15f9eac
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-04-08 15:05:01 -0400

    Generic D3 chart to start off.

--- apps/download.js
+++ apps/download.js
@@ -1,7 +1,7 @@
 var https = require('https');
 // https://www.rescuetime.com/anapi/manage
 var key = 'B63SNgMEIKmZXl3dhYh9FXYw6kcaCyztonT9HlUa';
-var url = 'https://www.rescuetime.com/anapi/data?key=' + key + '&format=csv&pv=interval&rs=hour&rb=2014-03-01';
+var url = 'https://www.rescuetime.com/anapi/data?key=' + key + '&format=csv&pv=interval&rs=hour&rb=2014-03-01&re=2014-04-07';
 
 https.get(url, function (res) {
   // console.log('status code \n', res.statusCode);
