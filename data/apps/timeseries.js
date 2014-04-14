616a5e8187b4225743e22a8c2b5d6b0c162fddcb
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-04-14 15:37:57 -0400

    Add x basic x axis.

--- data/apps/timeseries.js
+++ data/apps/timeseries.js
@@ -1,31 +1,48 @@
-var width = 420,
-    barHeight = 20;
-var x = d3.scale.linear()
-    .range([0, width]);
+var margin = {top: 20, right: 30, bottom: 20, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+var y = d3.scale.linear()
+    .range([height, 0]);
 var chart = d3.select(".chart")
-    .attr("width", width);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("apps-hourly-20140304-20140407.csv", type, function(error, data) {
+d3.csv("apps-hourly.csv", type, function(error, data) {
   if (error) throw(error);
-  x.domain([0, d3.max(data, function(d) {
-    return d.duration;
-  })]);
-  chart.attr("height", barHeight * data.length);
+
+  var x = d3.scale.linear()
+      .domain([0, width])
+      .range([0, width]);
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+  y.domain([0, d3.max(data, function (d) { return d.duration; })]);
+  var barWidth = width / data.length;
+
   var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
       .attr("transform", function(d, i) {
-        return "translate(0," + i * barHeight + ")";
+        return "translate(" + i * barWidth + ", 0)";
       });
+
   bar.append("rect")
-      .attr("width", function(d) {
-        return x(d.duration);
+      .attr("y", function (d) { return y(d.duration); })
+      .attr("height", function(d) {
+        return height - y(d.duration);
       })
-      .attr("height", barHeight - 1);
+      .attr("width", barWidth - 1);
+
   bar.append("text")
-      .attr("x", function(d) { return x(d.duration) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
+      .attr("y", function(d) { return y(d.duration) + 3; })
+      .attr("x", barWidth / 2)
+      .attr("dy", ".75em")
       .text(function(d) { return d.duration; });
 });
 
