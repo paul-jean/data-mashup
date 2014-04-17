170c8838f5e4bffc8281c515750ca89d01480356
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-04-17 00:09:44 -0400

    Basic line plot working. Axis needs labels and better tick labels.

--- data/sleep/sleep.js
+++ data/sleep/sleep.js
@@ -1,17 +1,12 @@
-var height = 500,
-    width = 500;
+var margin = {top:20, right:20, bottom:30, left:50};
+var height = 500 - margin.top - margin.bottom,
+    width = 900 - margin.left - margin.right;
 
-var svg = d3.select('body').append('svg');
-
-svg.attr('height', height)
-    .attr('width', width);
-/*
-svg.append('text')
-    .attr('x', 10)
-    .attr('y', 20)
-    .text('hello world')
-    .attr('fill', 'blue');
-*/
+var svg = d3.select('body').append('svg')
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('width', width + margin.left + margin.right)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.json("./sleep-quality.json", function(err, json) {
   var events = json[0].events;
@@ -27,13 +22,30 @@ d3.json("./sleep-quality.json", function(err, json) {
   var y = d3.scale.linear()
       .range([height, 0])
       .domain([0, d3.max(timeseries, function(d) { return d[1]; })]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
   var line = d3.svg.line()
       .interpolate("linear")
       .x(function(d) { return x(d[0]); })
       .y(function(d) { return y(d[1]); });
   svg.append("path")
-      .attr("d", line(timeseries))
-      .attr("style", "fill:none; stroke:steelblue; stroke-width:1.5px");
+      .datum(timeseries)
+      .attr("class", "line")
+      .attr("d", line);
 
   var circles = svg.selectAll("g")
       .data(timeseries)
