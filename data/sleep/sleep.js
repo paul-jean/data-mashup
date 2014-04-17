0e73ef6a3471e387b7ad72a865caaf2afdf77555
Paul-Jean Letourneau <paul.jean.letourneau@gmail.com>
2014-04-17 00:40:07 -0400

    Add axes labels.

--- data/sleep/sleep.js
+++ data/sleep/sleep.js
@@ -1,4 +1,4 @@
-var margin = {top:20, right:20, bottom:30, left:50};
+var margin = {top:20, right:20, bottom:50, left:50};
 var height = 500 - margin.top - margin.bottom,
     width = 900 - margin.left - margin.right;
 
@@ -33,10 +33,21 @@ d3.json("./sleep-quality.json", function(err, json) {
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+      .call(xAxis)
+      .append("text")
+      .attr("transform", "translate(" + width/2 + "," + 30 + ")")
+      .style("text-anchor", "end")
+      .text("Time (seconds)");
+
   svg.append("g")
       .attr("class", "y axis")
-      .call(yAxis);
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Movement (unknown units)");
 
   var line = d3.svg.line()
       .interpolate("linear")
